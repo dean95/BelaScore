5fe4bd105127c065ff8dd09fe14c2ff76969c81c
Dean Djermanovic <dean.djermanovic@five.agency>
2024-08-10 18:37:55 +0200

    Remove team dividers

--- composeApp/src/commonMain/kotlin/com/belascore/score/ui/components/ScoreContent.kt
+++ composeApp/src/commonMain/kotlin/com/belascore/score/ui/components/ScoreContent.kt
@@ -1,8 +1,6 @@
 package com.belascore.score.ui.components
 
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.belascore.score.ui.ScoreUiState
@@ -13,15 +11,11 @@ fun ScoreContent(
     modifier: Modifier = Modifier
 ) {
     Row(modifier = modifier) {
-        scoreUiState.teams.forEachIndexed { index, team ->
+        scoreUiState.teams.forEach { team ->
             TeamScoreColumn(
                 team = team,
                 rounds = scoreUiState.rounds
             )
-
-            if (index < scoreUiState.teams.lastIndex) {
-                VerticalDivider(modifier = Modifier.fillMaxHeight())
-            }
         }
     }
 }
